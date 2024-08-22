bd76555862e7be5f8f5da0f8498692c06685272a
Hugo Tausch <hugo@HUGO3144>
2024-08-22 20:33:17 +0200

    add benchmark

---
+++ RomanNumbers.Benchmark/Program.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using RomanNumbersConverter;
+
+[RPlotExporter]
+public class Converters
+{
+    [Benchmark]
+    public int Backwords() => RomanNumberConverter.ConvertBackwords("MMXIII");
+}
+public class RomanNumbersBenchmark
+{
+
+    static void Main()
+    {
+        var summary = BenchmarkRunner.Run<Converters>();
+    }
+
+}
